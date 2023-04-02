27e68b7ecd4c23486b6e25486ac788740702db91
RussellOsiemo <russelosiemo@gmail.com>
2023-04-02 22:44:22 +0300

    final code

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,214 +1,97 @@
+import 'dart:js';
+
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:sajili/screens/home_screen.dart';
-import 'package:sajili/screens/signup_screen.dart';
+import 'package:sajili/screens/login_screen.dart';
+import 'package:sajili/helper/firebase_auth.dart';
 
-import '../helper/firebase_auth.dart';
-import '../helper/validator.dart';
+class HomeScreen extends StatefulWidget {
+  final User user;
+  HomeScreen({required this.user});
 
-class LoginScreen extends StatefulWidget {
   @override
-  _LoginScreenState createState() => _LoginScreenState();
+  _HomeScreenState createState() => _HomeScreenState();
 }
 
-class _LoginScreenState extends State<LoginScreen> {
-  final _formKey = GlobalKey<FormState>();
-
-  final _emailTextController = TextEditingController();
-  final _passwordTextController = TextEditingController();
-
-  final _focusEmail = FocusNode();
-  final _focusPassword = FocusNode();
-
-  bool _isProcessing = false;
-
-  Future<FirebaseApp> _initializeFirebase() async {
-    FirebaseApp firebaseApp = await Firebase.initializeApp();
-
-    User? user = FirebaseAuth.instance.currentUser;
-
-    if (user != null) {
-      Navigator.of(context).pushReplacement(
-        MaterialPageRoute(
-          builder: (context) => HomeScreen(
-            user: user,
-          ),
-        ),
-      );
-    }
-    return firebaseApp;
+class _HomeScreenState extends State<HomeScreen> {
+  late User _currentUser;
+  @override
+  void initState() {
+    _currentUser = widget.user;
+    super.initState();
   }
 
   @override
   Widget build(BuildContext context) {
-    return GestureDetector(
-      onTap: () {
-        _focusEmail.unfocus();
-        _focusPassword.unfocus();
-      },
-      child: Scaffold(
-        appBar: AppBar(
-          backgroundColor: Colors.deepPurpleAccent,
-          title: const Text('Sajili'),
-          centerTitle: true,
-        ),
-        body: FutureBuilder(
-          future: _initializeFirebase(),
-          builder: (context, snapshot) {
-            if (snapshot.connectionState == ConnectionState.done) {
-              return Padding(
-                  padding:
-                      const EdgeInsets.only(left: 24.0, right: 24.0, top: 48.0),
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.start,
-                    children: [
-                      SizedBox(
-                        height: 150,
-                        width: 150,
-                        child: Image.asset('stormseeker.jpg'),
-                      ),
-                      const Padding(
-                        padding: EdgeInsets.only(bottom: 30.0, top: 12.0),
-                        child: Text(
-                          'Welcome to Sajili',
-                          style: TextStyle(
-                            color: Colors.white,
-                            fontSize: 40.0,
-                          ),
-                        ),
-                      ),
-                      Form(
-                          key: _formKey,
-                          child: Column(
-                            children: <Widget>[
-                              TextFormField(
-                                controller: _emailTextController,
-                                focusNode: _focusEmail,
-                                validator: (value) => Validator.validateEmail(
-                                  email: value,
-                                ),
-                                decoration: InputDecoration(
-                                  hintText: 'Email Address',
-                                  errorBorder: UnderlineInputBorder(
-                                      borderRadius: BorderRadius.circular(6.0),
-                                      borderSide: BorderSide(
-                                        color: Colors.red,
-                                      )),
-                                ),
-                              ),
-                              SizedBox(height: 8.0),
-                              TextFormField(
-                                controller: _passwordTextController,
-                                focusNode: _focusPassword,
-                                validator: (value) =>
-                                    Validator.validatePassword(
-                                  password: value,
-                                ),
-                                decoration: InputDecoration(
-                                  hintText: 'Password',
-                                  errorBorder: UnderlineInputBorder(
-                                    borderRadius: BorderRadius.circular(6.0),
-                                    borderSide: BorderSide(
-                                      color: Colors.red,
-                                    ),
-                                  ),
-                                ),
-                              ),
-                              SizedBox(height: 24.0),
-                              _isProcessing
-                                  ? CircularProgressIndicator()
-                                  : Row(
-                                      mainAxisAlignment:
-                                          MainAxisAlignment.spaceBetween,
-                                      children: [
-                                        Expanded(
-                                          child: ElevatedButton(
-                                            onPressed: () async {
-                                              _focusEmail.unfocus();
-                                              _focusPassword.unfocus();
-
-                                              if (_formKey.currentState!
-                                                  .validate()) {
-                                                setState(() {
-                                                  _isProcessing = true;
-                                                });
-                                                User? user =
-                                                    await FirebaseAuthHelper
-                                                        .signInUsingEmailPassword(
-                                                  email:
-                                                      _emailTextController.text,
-                                                  password:
-                                                      _passwordTextController
-                                                          .text,
-                                                );
-                                                setState(() {
-                                                  _isProcessing = false;
-                                                });
-                                                if (user != null) {
-                                                  Navigator.of(context)
-                                                      .pushReplacement(
-                                                    MaterialPageRoute(
-                                                      builder: ((context) =>
-                                                          HomeScreem(
-                                                              user: user)),
-                                                    ),
-                                                  );
-                                                }
-                                              }
-                                            },
-                                            child: Text(
-                                              'Sign in',
-                                              style: TextStyle(
-                                                color: Colors.white,
-                                              ),
-                                            ),
-                                            style: ButtonStyle(
-                                              backgroundColor:
-                                                  MaterialStateProperty.all(
-                                                      Colors.deepOrangeAccent),
-                                            ),
-                                          ),
-                                        ),
-                                        SizedBox(
-                                          width: 24.0,
-                                        ),
-                                        Expanded(
-                                          child: ElevatedButton(
-                                            onPressed: () {
-                                              Navigator.of(context).push(
-                                                MaterialPageRoute(
-                                                  builder: (context) =>
-                                                      SignUpScreen,
-                                                ),
-                                              );
-                                            },
-                                            child: Text(
-                                              'SignUp',
-                                              style: TextStyle(
-                                                  color: Colors.white),
-                                            ),
-                                            style: ButtonStyle(
-                                              backgroundColor:
-                                                  MaterialStateProperty.all(
-                                                      Colors.deepOrangeAccent),
-                                            ),
-                                          ),
-                                        ),
-                                      ],
-                                    ),
-                            ],
-                          ))
-                    ],
-                  ));
-            }
-
-            return const Center(
-              child: CircularProgressIndicator(),
-            );
-          },
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.deepPurpleAccent,
+        title: Text("Karibu Nyumbani"),
+        centerTitle: true,
+      ),
+      body: WillPopScope(
+        onWillPop: () async {
+          final logout = await showDialog(
+            context: context,
+            builder: (context) {
+              return AlertDialog(
+                title: new Text('Are you sure?'),
+                content: new Text("Do you want to logout from the App?"),
+                actionsAlignment: MainAxisAlignment.spaceBetween,
+                actions: [
+                  TextButton(
+                    onPressed: () {
+                      Logout();
+                    },
+                    child: const Text('Yes'),
+                  ),
+                  TextButton(
+                    onPressed: () {
+                      Navigator.pop(context, false);
+                    },
+                    child: const Text('No'),
+                  ),
+                ],
+              );
+            },
+          );
+          return logout!;
+        },
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                'Name: ${_currentUser.displayName}',
+                style: Theme.of(context).textTheme.bodyText1,
+              ),
+              SizedBox(height: 16.0),
+              ElevatedButton(
+                onPressed: () async {
+                  await FirebaseAuth.instance.signOut();
+                  Navigator.of(context).pushReplacement(
+                    MaterialPageRoute(
+                      builder: (context) => LoginScreen(),
+                    ),
+                  );
+                },
+                child: const Text('Sign Out'),
+                style: ButtonStyle(
+                  backgroundColor: MaterialStateProperty.all(Colors.redAccent),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
+  }
+
+  Future<dynamic> Logout() async {
+    await FirebaseAuth.instance.signOut();
+
+    Navigator.of(context).pushReplacement(MaterialPageRoute(
+      builder: (context) => LoginScreen(),
+    ));
   }
 }
